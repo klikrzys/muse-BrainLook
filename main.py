6195c09706a9baa92ce82e5e848a2d820b5c4e9c
klikrzys <klikrzys@gmail.com>
2017-12-17 19:47:43 +0100

    Changed lib for open sound control

--- main.py
+++ main.py
@@ -1,52 +1,87 @@
+#!/usr/bin/python3
 import argparse
 import math
 
-from threading import Thread
+import threading
 
-import cherrypy
+def keypress(): 
+    """
+    Waits for the user to press a key. Returns the ascii code 
+    for the key pressed or zero for a function key pressed.
+    """                             
+    import msvcrt               
+    while 1:
+        if msvcrt.kbhit():              # Key pressed?
+            a = ord(msvcrt.getch())     # get first byte of keyscan code     
+            if a == 0 or a == 224:      # is it a function key?
+                msvcrt.getch()          # discard second byte of key scan code
+                return 0                # return 0
+            else:
+                return a                # else return ascii code
 
-import time
-import moment
-from datetime import datetime
+
+def eeg_calmnessBuffer(self, unused_addr, calmness):
+	print(calmness)
+
+# def eeg_batteryStatusBuffer( self, unused_addr, StateOfCharge, FuelGaugeBatteryVoltage, ADCBatteryVoltage, Temperature ):
+	# percentageBattery = round(StateOfCharge/100) 
+	# print(percentageBattery)
+
+# def connCheck( self, unused_addr, args, ch1, ch2, ch3, ch4 ):
+	# pass 
+
+# def eeg_connStatusBuffer( self, unused_addr, int ):
+	# print("Connection in bytes per second: "+str(int))
 
 
-from OSC import OSCServer
-import sys
-from time import sleep
+	
+	
+class oscServerThread(threading.Thread):
+	def __init__(self, threadID, name, ip, port):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.ip = ip
+		self.port = port
+		self._stop_event = threading.Event()
+	
+	def run(self):
+		from pythonosc import dispatcher
+		from pythonosc import osc_server
 
+		parser = argparse.ArgumentParser()
 
+		parser.add_argument("--ip", default=self.ip, help="The ip to listen on")
+
+		parser.add_argument("--port", type=int, default=self.port, help="The port to listen on")
+
+		args = parser.parse_args()
+
+		dispatcher = dispatcher.Dispatcher()
+		dispatcher.map("/muse/elements/experimental/mellow", print)
+
+		server = osc_server.ThreadingOSCUDPServer( (args.ip, args.port), dispatcher )
+		print("Serving on {}".format(server.server_address))
+		server.serve_forever()
+	
+	def stop(self):
+		self._stop_event.set()
+
+	def stopped(self):
+		return self._stop_event.is_set()
+
+	
 if __name__ == "__main__":
-	oscServer = oscServer()
-
-	MuseIoAdress = "127.0.0.1"
-	MuseIoPort = 5000
-
-	server = OSCServer( (MuseIoAdress, MuseIoPort) )
-	server.timeout = 0
-	run = True
-
-	# this method of reporting timeouts only works by convention
-	# that before calling handle_request() field .timed_out is 
-	# set to False
-	def handle_timeout(self):
-		self.timed_out = True
-
-	# funny python's way to add a method to an instance of a class
-	import types
-	server.handle_timeout = types.MethodType(handle_timeout, server)
+	print("Aby wyłączyć naciśnij q albo klawisz ESC")
 	
-	server.addMsgHandler( "/muse/elements/experimental/mellow", self.eeg_calmnessBuffer )
+	# Init Threads
+	OSCServerThread = oscServerThread(1, "OSCServerThread", "127.0.0.1", 5000)
+
+	# Start Threads
+	OSCServerThread.start()
 	
-	def eeg_calmnessBuffer(self, unused_addr, calmness):
-		print(calmness)
-
-	# def eeg_batteryStatusBuffer( self, unused_addr, StateOfCharge, FuelGaugeBatteryVoltage, ADCBatteryVoltage, Temperature ):
-		# percentageBattery = round(StateOfCharge/100) 
-		# print(percentageBattery)
-
-	# def connCheck( self, unused_addr, args, ch1, ch2, ch3, ch4 ):
-		# pass 
-
-	# def eeg_connStatusBuffer( self, unused_addr, int ):
-		# print("Connection in bytes per second: "+str(int))
-	
+	# Wait for exit sign
+	x = input()
+	if x == "q":
+		OSCServerThread.stop()
+		return 0
